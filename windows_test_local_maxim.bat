38783538ce46f4f8ed333bd7829f90cb53180d91
Maxim Vochten <maxim.vochten@kuleuven.be>
2023-10-24 12:48:56 +0200

    remove other languages

--- windows_test_local_maxim.bat
+++ windows_test_local_maxim.bat
@@ -1,5 +1,5 @@
 set PATH=%PATH%;C:\Workdir\Programs\Hugo\hugo_extended_0.119.0_windows-amd64
-start http://localhost:1313/
+start http://localhost:1313/public_code/invariants_website/
 REM run 'hugo' to write the public/ folder to disk, otherwise it is just kept in RAM memory
 hugo server --disableFastRender
 pause
